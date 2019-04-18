fixed gradients wrt A and b

dreg_estimators/toy_example.py
@@ -94,47 +94,59 @@ class ToyConditionalNormal(object):
 
   def condition(self, tensor_list):
     """Computes the parameters of a normal distribution based on the inputs."""
-    # # Remove None's from tensor_list
 
+    # # In EAGER Mode, tensor_list is now an ndarray :(
     if isinstance(tensor_list, np.ndarray):
         tensor_list = [t.item() for t in tensor_list]
+        input_dim = self.size
+        raw_input_shape = tf.shape(tensor_list)
+        fcnet_input_shape = [tf.reduce_prod(raw_input_shape), input_dim]
+        fcnet_inputs = tf.reshape(tensor_list, fcnet_input_shape)
+        print("fcnet_inputs shape:", fcnet_inputs.shape)
+        out = self.fcnet(fcnet_inputs)
     else:
-        tensor_list = [t[0].item() for t in tensor_list[0]]
+        print("CONDITION ON AN NDARRAY")
+        # helpful commands if tensor_list is again a tensor
+        # tensor_list = [t[0].item() for t in tensor_list]
+        # # Reshape outputs to the original shape.
+        # output_size = tf.concat([raw_input_shape, [1]], axis=0)
+        # out = tf.reshape(outs, output_size)
 
-    # # Hacked this a bit for a list of ndarray inputs
-    # concatted_inputs = tf.concat(tensor_list, axis=-1)
-    input_dim = self.size
-    raw_input_shape = tf.shape(tensor_list)
-    fcnet_input_shape = [tf.reduce_prod(raw_input_shape), input_dim]
-    fcnet_inputs = tf.reshape(tensor_list, fcnet_input_shape)
-    print("fcnet_inputs shape:", fcnet_inputs.shape)
-    outs = self.fcnet(fcnet_inputs)
-    return outs
+    # print("1. ", len(tensor_list))
+    # print("3. ", input_dim)
+    # print("4. ", raw_input_shape)
+    # print("5. ", fcnet_input_shape)
+    # print("6. ", fcnet_inputs.shape)
+    # print("input ", fcnet_inputs)
+    # print("7. ", out)
 
-  def initialise_network(self, some_data):
+    return out
+
+  def initialise_and_fix_network(self, some_data):
+
       mu = self.condition(some_data)
-      self.fcnet.get_variables()
-
       # Set the network parameters to "close" to their optimal
       noiseA = np.random.normal(loc=0, scale=0.01, size=(FLAGS.latent_dim, 1)).astype(np.float32)
       noiseb = np.random.normal(loc=0, scale=0.01, size=(FLAGS.latent_dim,)).astype(np.float32)
 
-      A = np.ones(FLAGS.latent_dim) / 2. #+ noiseA
-      b = np.eye(FLAGS.latent_dim) * (2 / 2.) #+ noiseb
+      A = np.ones(FLAGS.latent_dim) / 2. + noiseA
+      b = np.eye(FLAGS.latent_dim) * (2 / 2.) + noiseb
       tf_A = tf.Variable(A.astype(np.float32).reshape(FLAGS.latent_dim, FLAGS.latent_dim))
       tf_b = tf.Variable(b.astype(np.float32).reshape(FLAGS.latent_dim, ))
 
       self.fcnet.get_variables()[0].assign(tf_A)
       self.fcnet.get_variables()[1].assign(tf_b)
 
+      print(self.fcnet.get_variables())
 
   def __call__(self, *args, **kwargs):
     """Creates a normal distribution conditioned on the inputs."""
 
-    mu1 = self.condition(args)
-    mu = tf.Variable(name="mu", initial_value=mu1)
+    mu = self.condition(args[0])
+    # mu = tf.Variable(name="mu", initial_value=) # This breaks the gradients :S
 
     if kwargs.get("stop_gradient", False):
+        print("STOP GRADIENTS")
         mu = tf.stop_gradient(mu)
     return tfd.Normal(loc=mu, scale=tf.eye(self.size)*(2/3.)), self.fcnet.get_variables()
 
@@ -161,11 +173,12 @@ def toy_example():
             name="proposal")
 
         # initialise the network parameters to near optimal
-        q_z.initialise_network(batch_xs)
+        q_z.initialise_and_fix_network(batch_xs)
 
         # returns the Normal dist proposal, and the parameters (fixed to optimal A and b)
         proposal, inference_network_params = q_z(batch_xs, stop_gradient=False)
         z = proposal.sample(sample_shape=[FLAGS.num_samples])
+        # [num_samples, batch_size, latent_dim]
         print("samples ", z.shape)
 
         # returns a Normal dist conditioned on z
@@ -175,45 +188,41 @@ def toy_example():
         prior, mu = p_z()
 
         # merge the parameters to compute gradients wrt
-        # parameters = (inference_network_params[0], inference_network_params[1], mu)
+        parameters = (inference_network_params[0], inference_network_params[1], mu)
 
         log_p_z = tf.reduce_sum(prior.log_prob(z), axis=-1)
         log_q_z = tf.reduce_sum(proposal.log_prob(z), axis=-1)
         log_p_x_given_z = tf.reduce_sum(likelihood.log_prob(batch_xs), axis=-1)
-
         log_weights = log_p_z + log_p_x_given_z - log_q_z
         log_sum_weight = tf.reduce_logsumexp(log_weights, axis=0)  # this converts back to IWAE estimator (log of the sum)
         log_avg_weight = log_sum_weight - tf.log(tf.to_float(FLAGS.num_samples))
 
-        # Build the evidence lower bound (ELBO) or the negative loss
-        kl = tf.reduce_mean(tfd.kl_divergence(proposal, prior), axis=-1)  # analytic KL
-        log_sum_ll = tf.reduce_logsumexp(log_p_x_given_z, axis=0)  # this converts back to IWAE estimator (log of the sum)
-        expected_log_likelihood = log_sum_ll - tf.log(tf.to_float(FLAGS.num_samples))
-        KL_elbo = tf.reduce_mean(expected_log_likelihood - kl)
+        # # Build the evidence lower bound (ELBO) or the negative loss
+        # kl = tf.reduce_mean(tfd.kl_divergence(proposal, prior), axis=-1)  # analytic KL
+        # log_sum_ll = tf.reduce_logsumexp(log_p_x_given_z, axis=0)  # this converts back to IWAE estimator (log of the sum)
+        # expected_log_likelihood = log_sum_ll - tf.log(tf.to_float(FLAGS.num_samples))
+        # KL_elbo = tf.reduce_mean(expected_log_likelihood - kl)
 
-        model_loss = -tf.reduce_mean(log_avg_weight)
+        # model_loss = -tf.reduce_mean(log_avg_weight)
         inference_loss = -tf.reduce_mean(log_avg_weight)
-        log_p_hat_mean = tf.reduce_mean(log_avg_weight)
+        # log_p_hat_mean = tf.reduce_mean(log_avg_weight)
         print("inference_loss ", inference_loss)
 
-    grads = tape.gradient(inference_loss, inference_network_params)
+    grads = tape.gradient(inference_loss, parameters)
+    vectorized_grads = [np.array(g).reshape(1,) for g in grads]
+    print("GRADIENTS = ", vectorized_grads)
 
-    # inference_grads = grads[:2]
-    print(">", grads)
+    # first_moments = np.average(vectorized_grads)
+    # print("first_moments  = ", first_moments )
+    # second_moments = np.average(tf.square(vectorized_grads))
+    # print("second_moments  = ", second_moments)
+    #
+    # variances = second_moments - tf.square(first_moments)
+    # inference_grad_snr_sq = tf.reduce_mean(tf.square(first_moments)) / tf.reduce_mean(variances)
+    # snr = tf.reduce_mean(tf.square(first_moments)) / tf.reduce_mean(variances)
+    #
+    # print("SNR = ", snr)
 
-    generator_grads = grads[2:]
-
-    vectorized_grads = tf.concat([tf.reshape(g, [-1]) for g in inference_grads if g is not None], axis=0)
-    first_moments = np.average(vectorized_grads)
-    second_moments = np.average(tf.square(vectorized_grads))
-    variances = second_moments - tf.square(first_moments)
-    inference_grad_snr_sq = tf.reduce_mean(tf.square(first_moments)) / tf.reduce_mean(variances)
-
-    print("grads = ", vectorized_grads)
-    print("1st moments  = ", first_moments)
-    print("2nd moments  = ", second_moments)
-    print("var ", variances)
-    print("SNR ", inference_grad_snr_sq)
 
 if __name__ == "__main__":
 